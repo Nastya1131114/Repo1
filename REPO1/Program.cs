3609583fa3f340c43942afb99118807bacd34c70
Your Name <anastasia29082004@yandex.ru>
2024-03-30 15:00:29 +0500

    repo1

--- REPO1/Program.cs
+++ REPO1/Program.cs
@@ -1,24 +1,4 @@
-﻿//using System;
-
-//public class IsLeapYear
-//{
-//    public static void Main()
-//    {
-//        for (int year = 1994; year <= 3000; year++)
-//        {
-//            if (DateTime.IsLeapYear(year))
-//            {
-//                Console.WriteLine("{0} is a leap year.", year);
-//                DateTime leapDay = new DateTime(year, 2, 29);
-//                DateTime nextYear = leapDay.AddYears(1);
-//                Console.WriteLine("   One year from {0} is {1}.",
-//                                  leapDay.ToString("d"),
-//                                  nextYear.ToString("d"));
-//            }
-//        }
-//    }
-//}
-using System;
+﻿using System;
 
 class LeapYearChecker
 {
@@ -27,7 +7,7 @@ class LeapYearChecker
         Console.Write("Введите год: ");
         int year = Convert.ToInt32(Console.ReadLine());
 
-        if (year % 4 == 0/*; year % 100 != 0) (year % 400 == 0))*/)
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // &&- и ; ||- или
         {
             Console.WriteLine(year + "  — високосный год.");
         }
